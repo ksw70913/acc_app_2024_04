e9fc0da542f6c1a9fc7b49f925b16737aae439a0
ksw70913 <ksw70913@gmail.com>
2024-04-26 16:12:28 +0900

    스프링 시큐리티, 로그인, 로그아웃, 회원가입 1

--- src/main/java/com/koreait/exam/acc_app_2024_04/controller/MemberController.java
+++
@@ -1,4 +0,0 @@
-package com.koreait.exam.acc_app_2024_04.controller;
-
-public class MemberController {
-}
--- src/main/java/com/koreait/exam/acc_app_2024_04/repository/MemberRepository.java
+++
@@ -1,4 +0,0 @@
-package com.koreait.exam.acc_app_2024_04.repository;
-
-public interface MemberRepository {
-}
